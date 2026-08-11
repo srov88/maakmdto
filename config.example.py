91f116c3799936174a3f4489ae040b3afac47bd4
srov88 <git.safeguard391@passmail.net>
2026-08-11 10:21:27 +0200

    Uitleg over gebruik config.example.py toegevoegd

--- config.example.py
+++ config.example.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+# Dit in een voorbeeld bestand. Voor gebruik in maakmdto moet de naam nog even aangepast worden naar config.py
+# Vervolgens kan je config.py toevoegen aan je .gitignore
+
 
 #vul hier paden in 
 bronMap = Path(r"pad\naar\bron")
